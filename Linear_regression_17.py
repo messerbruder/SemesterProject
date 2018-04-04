bc901170da57a92345ecd52b46ab66babc025d3b
dauye <32542635+messerbruder@users.noreply.github.com>
2018-04-04 15:37:39 +0200

    Add c1-c9
    Add prediction between max and min cloudiness  (shitty code as usual)

--- Linear_regression_17.py
+++ Linear_regression_17.py
@@ -40,12 +40,13 @@ lm = linear_model.LinearRegression()
 
 filepath = 'D:/SemesterProject/Radiation_Record/'
 
-non_zero = 'Radiation_regression.csv'
+file_name = 'Radiation_regression.csv'
 
 
 
 
-LookupTable = open(filepath + 'panel'+ str(panel_number) + non_zero, 'w') #reset or create file
+LookupTable = open(filepath + 'panel'+ str(panel_number) + file_name, 'w') #reset or create file
+LookupTable.write('Look up group, Hour, Max Radiation, c1, c2, c3, c4, c5, c6, c7, c8, c9, Min Radiation \n')
 LookupTable.close()
 
 index = 0 # hour of year
@@ -74,11 +75,23 @@ while index < 5780: #20groups*17days*17hours
 
     lm.fit(X,y)
     Max_radiation = lm.predict(0) # generate prediction when sky is clear (cloudiness = 0)
+    c1 = lm.predict(1)
+    c2 = lm.predict(2)
+    c3 = lm.predict(3)
+    c4 = lm.predict(4)
+    c5 = lm.predict(5)
+    c6 = lm.predict(6)
+    c7 = lm.predict(7)
+    c8 = lm.predict(8)
+    c9 = lm.predict(9)
+    Min_radiation = lm.predict(10)  # predict when cloudiness = 10
     display_hour = index%17+6
     #print('index = '+str(index))
 
-    LookupTable = open(filepath + 'panel' + str(panel_number) + non_zero, 'a')
-    LookupTable.write(str(lrGroup) + ',' + str(display_hour) + ',' + str(Max_radiation[0][0]) +  '\n')
+    LookupTable = open(filepath + 'panel' + str(panel_number) + file_name, 'a')
+    LookupTable.write(str(lrGroup) + ',' + str(display_hour) + ',' + str(Max_radiation[0][0]) + ',' +
+                      str(c1[0][0]) + ',' + str(c2[0][0]) + ',' + str(c3[0][0]) + ',' + str(c4[0][0]) + ',' + str(c5[0][0]) + ',' +
+                      str(c6[0][0]) + ',' + str(c7[0][0]) + ',' + str(c8[0][0]) + ',' + str(c9[0][0]) + ',' + str(Min_radiation[0][0]) +  '\n')
     if index % 17 + 6 == 22:
         index += 290 # 289 (17*17)+1 = 290
     else:
@@ -93,7 +106,7 @@ while dolast == True:
     lrGroup = 21
 
     pickList = np.arange(start=index,stop=(index)+17*25, step=17)
-    print(pickList)
+    #print(pickList)
     trad = merge_table.iloc[pickList,4]
     tcod = merge_table.iloc[pickList,5]
     cloud_hourly = pd.DataFrame(tcod)
@@ -105,13 +118,27 @@ while dolast == True:
 
     lm.fit(X,y)
     Max_radiation = lm.predict(0) # predict when cloudiness = 0
-
+    c1 = lm.predict(1)
+    c2 = lm.predict(2)
+    c3 = lm.predict(3)
+    c4 = lm.predict(4)
+    c5 = lm.predict(5)
+    c6 = lm.predict(6)
+    c7 = lm.predict(7)
+    c8 = lm.predict(8)
+    c9 = lm.predict(9)
+    Min_radiation = lm.predict(10) #predict when cloudiness = 10
+    print(Max_radiation)
+    print('.....')
+    print(Min_radiation)
 
     display_hour = index%17+6
     #print('index = '+str(index))
 
-    LookupTable = open(filepath + 'panel' + str(panel_number) + non_zero, 'a')
-    LookupTable.write(str(lrGroup) + ',' + str(display_hour) + ',' + str(Max_radiation[0][0]) +  '\n')
+    LookupTable = open(filepath + 'panel' + str(panel_number) + file_name, 'a')
+    LookupTable.write(str(lrGroup) + ',' + str(display_hour) + ',' + str(Max_radiation[0][0]) + ',' +
+                      str(c1[0][0]) + ',' + str(c2[0][0]) + ',' + str(c3[0][0]) + ',' + str(c4[0][0]) + ',' + str(c5[0][0]) + ',' +
+                      str(c6[0][0]) + ',' + str(c7[0][0]) + ',' + str(c8[0][0]) + ',' + str(c9[0][0]) + ',' + str(Min_radiation[0][0]) +  '\n')
 
     if index % 17+6 == 22:
         dolast = False
